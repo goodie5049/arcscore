dc5d40c6a7ea26f3417d10aceb20c594b2562752
goodie5049 <onyeomankem666@gmail.com>
2026-03-31 02:18:31 +0100

    fix: switch to direct Arc Testnet RPC

--- lib/arc.ts
+++ lib/arc.ts
@@ -3,7 +3,7 @@ import { arcTestnet } from "viem/chains";
 
 export const publicClient = createPublicClient({
   chain: arcTestnet,
-  transport: http("https://5042002.rpc.thirdweb.com"),
+  transport: http("https://rpc.testnet.arc.network"),
 });
 
 export const IDENTITY_REGISTRY =
